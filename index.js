df9d4cd0cf8d7e93eaa6000010a040c4ee6fd40d
Joshua <yoonj200@gmail.com>
2022-02-03 20:02:03 -0600

    commit tree 86f56d8bf7a571decc84ab77c90e1a15f7b37482

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+//declare customerName to be bob in global scope
+// 1) returns the customerName
+var customerName = `bob`;
+
+//upperCaseCustomerName()
+//2) modifies the customerName variable
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+//setBestCustomer()
+//3) setBestCustomer
+function setBestCustomer() {
+    bestCustomer = `not bob`;
+    return bestCustomer;
+}
+
+//overwriteBestCustomer()
+//4) overwrites the best customer
+function overwriteBestCustomer() {
+    bestCustomer = `maybe bob`;
+}
+
+//changeLeastFavoriteCustomer()
+//5) unsuccessfully tries to reassign the least favorite customer
+const leastFavoriteCustomer = `sometimes bob`;
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = `definitely bob`;    
+}
